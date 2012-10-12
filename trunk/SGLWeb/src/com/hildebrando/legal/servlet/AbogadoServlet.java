55eab12c3570ad9346714c12fe0a686a48d62e59
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2012-10-12 22:06:57 +0000

    [12-10-2012] SGLWEB: Llamada a aplicacion de Mantenimientos.

---
+++ trunk/SGLWeb/src/com/hildebrando/legal/servlet/AbogadoServlet.java
@@ -0,0 +1,25 @@
+package com.hildebrando.legal.servlet;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AbogadoServlet extends HttpServlet 
+{
+  public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+  {
+	   String requestURL = req.getRequestURL().toString();
+	   String servletPath = req.getServletPath();
+	   String serverPath = requestURL.substring(0,requestURL.indexOf(servletPath));
+	   
+	   String nuevoPath = serverPath.replace("SGLWeb", "");
+	   System.out.println("URL: " + nuevoPath);
+	   
+	   URL url = new URL("http://localhost:8084/Mantenimientos/modules/Abogado");
+	   
+	   res.sendRedirect(url.getPath());
+  }
+}
