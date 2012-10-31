2e91d6b22987e70ca08787bbf07f2d7d8a82085c
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2012-10-31 16:47:34 +0000

    [31-10-2012] SGLWEB: Hito 4

---
+++ trunk/SGLWeb/src/com/bbva/general/entities/Generico.java
@@ -0,0 +1,22 @@
+package com.bbva.general.entities;
+
+public class Generico {
+	
+private String descripcion;
+private String cantidad;
+public String getDescripcion() {
+	return descripcion;
+}
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+public String getCantidad() {
+	return cantidad;
+}
+public void setCantidad(String cantidad) {
+	this.cantidad = cantidad;
+}
+
+
+
+}
