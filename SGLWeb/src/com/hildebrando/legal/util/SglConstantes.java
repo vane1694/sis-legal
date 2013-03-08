8dcf18fa788b51423d0458894a9ab1bb9a2169c1
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-03-08 23:13:36 +0000

    [08.03.2013] Inclusion de mensajes log en clase.

--- SGLWeb/src/com/hildebrando/legal/util/SglConstantes.java
+++ SGLWeb/src/com/hildebrando/legal/util/SglConstantes.java
@@ -18,5 +18,18 @@ public class SglConstantes {
 	
 	public static final String MODO_LECTURA = "0";
 	public static final String MODO_EDICION = "1";
+
+	public static final String MSJ_ERROR_OBTENER="Ha ocurrido un error al obtener ";
+	public static final String MSJ_ERROR_EXCEPTION="Ha ocurrido una excepcion ";
+	public static final String MSJ_ERROR_CONSULTAR="Ha ocurrido un error al consultar ";
+	public static final String MSJ_ERROR_REGISTR="Ha ocurrido un error al registrar ";
+	public static final String MSJ_ERROR_ELIMIN="Ha ocurrido un error al eliminar ";
+	public static final String MSJ_ERROR_MODIF="Ha ocurrido un error al modificar ";
+	public static final String MSJ_ERROR_ACTUALIZ="Ha ocurrido un error al actualizar ";
+	public static final String MSJ_ERROR_SESSION_USU="El usuario sesion no existe, es nulo.";
+	
+	public static final String MSJ_TAMANHIO_LISTA="El tamaño de la lista ";
+	public static final String MSJ_EXITO_OBTENER="Se obtuvo exitosamente ";
+
 }
 
