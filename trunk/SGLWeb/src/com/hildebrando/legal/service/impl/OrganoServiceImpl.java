096d3df15f6a275bd2132fb67115c59c0c56d127
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-03-14 16:04:14 +0000

    [14.03.2013] Inclusion de mensajes log en clase.

--- trunk/SGLWeb/src/com/hildebrando/legal/service/impl/OrganoServiceImpl.java
+++ trunk/SGLWeb/src/com/hildebrando/legal/service/impl/OrganoServiceImpl.java
@@ -1,31 +1,27 @@
 package com.hildebrando.legal.service.impl;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
+import org.apache.log4j.Logger;
 
 import com.bbva.common.listener.SpringInit.SpringInit;
 import com.bbva.persistencia.generica.dao.GenericDao;
 import com.hildebrando.legal.modelo.Organo;
 import com.hildebrando.legal.service.OrganoService;
+import com.hildebrando.legal.util.SglConstantes;
 
 public class OrganoServiceImpl implements OrganoService {
 
+	public static Logger logger = Logger.getLogger(OrganoServiceImpl.class);
+	
 	@Override
-	public Organo registrar(Organo organo) {
-		
+	public Organo registrar(Organo organo) {		
 		Organo organobd=new Organo();
 		GenericDao<Organo, Object> organoDAO = (GenericDao<Organo, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
 		
-		
-		try {
-			
-			organobd= organoDAO.insertar(organo);
-			
-			return organobd;
-			
+		try {			
+			organobd= organoDAO.insertar(organo);			
+			return organobd;			
 		} catch (Exception e) {
-			e.printStackTrace();
-			
+			logger.error(SglConstantes.MSJ_ERROR_REGISTR+"el organo :" +e);			
 			return null;
 		}
 	}
--- trunk/SGLWeb/src/com/hildebrando/legal/service/impl/PersonaServiceImpl.java
+++ trunk/SGLWeb/src/com/hildebrando/legal/service/impl/PersonaServiceImpl.java
@@ -1,23 +1,24 @@
 package com.hildebrando.legal.service.impl;
 
+import org.apache.log4j.Logger;
+
 import com.bbva.common.listener.SpringInit.SpringInit;
 import com.bbva.persistencia.generica.dao.GenericDao;
-import com.hildebrando.legal.modelo.Abogado;
 import com.hildebrando.legal.modelo.Persona;
 import com.hildebrando.legal.service.PersonaService;
+import com.hildebrando.legal.util.SglConstantes;
 
 public class PersonaServiceImpl implements PersonaService {
 
+	public static Logger logger = Logger.getLogger(PersonaServiceImpl.class);
+	
 	@Override
 	public Persona registrar(Persona persona) {
 		GenericDao<Persona, Object> personaDAO = (GenericDao<Persona, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
-		
-		try {
-			
+		try {			
 			return personaDAO.insertar(persona);
-			
 		} catch (Exception e) {
-			
+			logger.error(SglConstantes.MSJ_ERROR_REGISTR+"la persona :" +e);
 			return null;
 		}
 	}
