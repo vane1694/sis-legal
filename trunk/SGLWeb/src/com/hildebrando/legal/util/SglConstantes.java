305018c74d343698a454be93d74e8a56049a560d
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-03-14 16:15:57 +0000

    [14.03.2013] Inclusion de mensajes log en clase.

--- trunk/SGLWeb/src/com/hildebrando/legal/util/SglConstantes.java
+++ trunk/SGLWeb/src/com/hildebrando/legal/util/SglConstantes.java
@@ -28,7 +28,7 @@ public class SglConstantes {
 	public static final String MSJ_ERROR_MODIF="Ha ocurrido un error al modificar ";
 	public static final String MSJ_ERROR_ACTUALIZ="Ha ocurrido un error al actualizar ";
 	public static final String MSJ_ERROR_SESSION_USU="El usuario sesion no existe, es nulo.";
-	
+	public static final String MSJ_ERROR_CONVERTIR="Ha ocurrido un error al convertir ";
 	public static final String MSJ_TAMANHIO_LISTA="El tamaño de la lista ";
 	public static final String MSJ_EXITO_OBTENER="Se obtuvo exitosamente ";
 
