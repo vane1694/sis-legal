24750f5e7c040b3326131f703300094cb946ce87
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-03-22 15:05:48 +0000

    [21.03.2013] Resolucion de incidencias.

--- SGLWeb/src/com/hildebrando/legal/view/NumeroExpedienteValidator.java
+++ SGLWeb/src/com/hildebrando/legal/view/NumeroExpedienteValidator.java
@@ -9,10 +9,13 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import org.apache.log4j.Logger;
 
 @FacesValidator("numeroExpedienteValidator")
 public class NumeroExpedienteValidator implements Validator {
 
+	public static Logger logger = Logger.getLogger(NumeroExpedienteValidator.class);
+	
 	private static final String ALFANUMERICO = "[A-Za-z0-9-_/*]+";
  
 	private Pattern pattern;
@@ -31,6 +34,7 @@ public class NumeroExpedienteValidator implements Validator {
 		if(!matcher.matches()){
 			FacesMessage msg = new FacesMessage("Inválido");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			logger.debug("Hay un error en ExpedienteValidator:"+msg);
 			throw new ValidatorException(msg);
 		}
 	}
