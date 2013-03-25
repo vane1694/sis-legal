ca210176f076d83b492391565f2e25a36e13f0ee
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-03-25 15:06:19 +0000

    [25.03.2013] Resolucion de incidencias.

--- trunk/SGLWeb/src/com/hildebrando/legal/view/UbigeoConverter.java
+++ trunk/SGLWeb/src/com/hildebrando/legal/view/UbigeoConverter.java
@@ -7,13 +7,18 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
+import org.apache.log4j.Logger;
+
 import com.bbva.common.listener.SpringInit.SpringInit;
 import com.bbva.persistencia.generica.dao.GenericDao;
 import com.hildebrando.legal.modelo.Ubigeo;
+import com.hildebrando.legal.util.SglConstantes;
 
 @FacesConverter(value="ubigeoConverter")
 public class UbigeoConverter implements Converter {
 
+	public static Logger logger = Logger.getLogger(UbigeoConverter.class);	
+	
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String value) {
 
@@ -33,15 +38,14 @@ public class UbigeoConverter implements Converter {
         				ubigeo.setDescripcionDistrito(texto);
         				
         			}
-        			
-        			
         			return ubigeo;
+        			
         		} catch (Exception e) {
-        			e.printStackTrace();
+        			logger.error(SglConstantes.MSJ_ERROR_EXCEPTION+"en UbigeoConverter:"+e);
         		}
   
             } catch(NumberFormatException exception) {  
-                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ubigeo invalido", "Ubigeo invalido"));  
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ubigeo inválido", "Ubigeo inválido"));  
             }  
         }  
 		 return null;  
