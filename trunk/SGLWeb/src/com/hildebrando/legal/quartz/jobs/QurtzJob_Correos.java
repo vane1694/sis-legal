4fa8c8493db21ff50d17676bd5307215931b6ab9
cesar0222@gmail.com <cesar0222@gmail.com@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-07-10 23:20:59 +0000

    10-07-13: Añadir tarea al job de correo por incidencia SGL

--- trunk/SGLWeb/src/com/hildebrando/legal/quartz/jobs/QurtzJob_Correos.java
+++ trunk/SGLWeb/src/com/hildebrando/legal/quartz/jobs/QurtzJob_Correos.java
@@ -12,6 +12,7 @@ public class QurtzJob_Correos implements Job {
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		EnvioMailMB mail = new EnvioMailMB();
 		mail.enviarCorreoCambioColor();	
+		mail.enviarCorreoCambioFechas();
 	}
 
 }
