498ab8512678578d364124c72f55bb45c6434aad
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-07-16 22:52:45 +0000

    16.07.2013 - Inclusión de logs en aplicación.

--- SGLWeb/src/com/hildebrando/legal/quartz/jobs/QuartzJob_TipoCambio.java
+++ SGLWeb/src/com/hildebrando/legal/quartz/jobs/QuartzJob_TipoCambio.java
@@ -14,12 +14,10 @@ public class QuartzJob_TipoCambio implements Job {
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		JobsMB job = new JobsMB();
-		 String patron = "yyyy-MM-dd";
-		 SimpleDateFormat formato = new SimpleDateFormat(patron);
-		//System.out.println("FEHCA FEHCA :"+formato.format(new Date()));
+		String patron = "yyyy-MM-dd";
+		SimpleDateFormat formato = new SimpleDateFormat(patron);
+		//System.out.println("Fecha: "+formato.format(new Date()));
 		String fechaString  = formato.format(new Date());   
 		job.cargarTipoCambio(fechaString, "S", "");
-		
 	}
-
 }
