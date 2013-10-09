8ef0c798241e369772d6a6ebe85532172b1fe6f3
jsaldana999 <jsaldana999@427869b9-49d3-7ff6-e4d4-80132b91c1e2>
2013-10-09 16:45:58 +0000

    [09.10.2013] Inclusion de logs y Refactor de clases

--- SGLWeb/src/com/hildebrando/legal/view/PersonaConverter.java
+++ SGLWeb/src/com/hildebrando/legal/view/PersonaConverter.java
@@ -11,15 +11,19 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
+import org.apache.log4j.Logger;
+
 import com.bbva.common.listener.SpringInit.SpringInit;
 import com.bbva.persistencia.generica.dao.Busqueda;
 import com.bbva.persistencia.generica.dao.GenericDao;
 import com.hildebrando.legal.modelo.Persona;
+import com.hildebrando.legal.util.SglConstantes;
 
 @FacesConverter(value="personaCvtr")
 public class PersonaConverter implements Converter, Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	public static Logger logger = Logger.getLogger(PersonaConverter.class);
 
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String value) {
@@ -30,7 +34,7 @@ public class PersonaConverter implements Converter, Serializable {
 		try {
 			personas = personaDAO.buscarDinamico(filtro);
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error(SglConstantes.MSJ_ERROR_EXCEPTION+"en PersonaConverter: ",e);
 		}
 		
 		
